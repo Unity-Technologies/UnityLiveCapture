1f7577f2c84db3ae6e6d9d067121718de5c37db4
SanaJal <104858438+SanaJal@users.noreply.github.com>
2023-07-26 16:49:04 -0400

    add urptest project

---
+++ TestProjects/URPTests/Assets/Tests/Editor/UrpCameraDriverTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using Unity.LiveCapture.VirtualCamera;
+using Unity.LiveCapture.Tests.Editor;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.TestTools;
+
+class UrpCameraDriverTests : BaseCameraDriverTests
+{
+    [UnityTest]
+    public IEnumerator TestFocusModeSynchronization()
+    {
+        var depthOfField = GetDepthOfField();
+        yield return TestFocusModeSynchronizationInternal(depthOfField);
+    }
+
+    DepthOfField GetDepthOfField()
+    {
+        var volume = m_GameObject.GetComponent<Volume>();
+        Assert.IsNotNull(volume);
+
+        var profile = volume.profile;
+        Assert.IsNotNull(volume);
+
+        Assert.IsTrue(profile.TryGet(out DepthOfField depthOfField));
+
+        return depthOfField;
+    }
+
+    protected override GameObject CreateVirtualCameraActor()
+    {
+        return new GameObject("Virtual Camera Actor", typeof(PhysicalCameraDriver));
+    }
+
+    protected override void ValidateLensAndBodySynchronization(Lens lens, LensIntrinsics instrinsics, CameraBody cameraBody)
+    {
+        var camera = m_GameObject.GetComponent<Camera>();
+        Assert.IsNotNull(camera);
+
+        ValidateCameraSynchronization(camera, lens, instrinsics, cameraBody);
+
+        var volume = m_GameObject.GetComponent<Volume>();
+        Assert.IsNotNull(volume);
+
+        var profile = volume.profile;
+        Assert.IsNotNull(volume);
+
+        Assert.IsTrue(profile.TryGet(out DepthOfField depthOfField));
+
+        Assert.IsTrue(Mathf.Approximately(lens.Aperture, depthOfField.aperture.value));
+        Assert.IsTrue(Mathf.Approximately(lens.FocalLength, depthOfField.focalLength.value));
+        Assert.IsTrue(Mathf.Approximately(instrinsics.BladeCount, depthOfField.bladeCount.value));
+        Assert.IsTrue(Mathf.Approximately(lens.FocusDistance, depthOfField.focusDistance.value));
+    }
+}
